b6f104074a5dc02b014c814a16af1f5de956618d
Avil <avilavate@live.in>
2016-02-26 20:35:43 +0530

    A saple graph app for showing relaive education/medication and population data of a simple graph.

---
+++ JSEvaluator/graph.js
@@ -0,0 +1,53 @@
+﻿
+angular.module("app", []).controller("ctrl", ["circleFactory", "$scope", function (circleFactory, $scope) {
+    $scope.circles = [];
+    for (var i = 0; i <= 1000; i = i + 50) {
+        $scope.circles.push(i);
+    }
+    $scope.Countries = [];
+    $scope.Circles = [];
+
+
+    $scope.addCountry = function (n, p, m, e) {
+        var c = new circleFactory.Country(n, p, m, e);
+        $scope.Circles.push(new circleFactory.Circle(c));
+    };
+}]);
+
+
+angular.module("app").factory("circleFactory", function () {
+    var Country = function (name, population, medication, education) {
+        this.population = population;
+        this.name = name;
+        this.medication = medication;
+        this.education = education;
+    };
+
+    var min = {
+        min: 1000000
+    };
+
+    var Circle = function (country) {
+        this.x = (country.education * 1000) / 100;
+        this.y = 1000 - (country.medication * 1000) / 100;
+        this.r = 0;
+        this.fitToSizeRadious = 0;
+        this.name = country.name;
+        this.radious = function () {
+            if (country.population !== undefined) {
+                //considers the minimum population as 1M using Pi*r*r formilae
+                this.r = Math.sqrt(((country.population) * 7) / 22) / 50;
+            }
+            return this.r;
+        };
+        this.fitToSize = function () {
+            this.fitToSizeRadious = this.r / min;
+        }
+    };
+
+    return {
+        Country: Country,
+        Circle: Circle
+    }
+});
+
---
+++ JSEvaluator/main.js
@@ -0,0 +1,110 @@
+﻿"use strict";
+
+angular.module("app", []).controller("ctrl", ["$scope","evalFactory", function ($scope, evalFactory) {
+    $scope.input;
+    $scope.isValid = false;
+   
+    $scope.ExpressionEvaluator = function () {
+        $scope.isValid = evalFactory.ExpressionEvaluator($scope.input);
+        $scope.result = evalFactory.Execute();
+    }
+
+
+}]).factory("evalFactory", function () {
+
+    var Stack = function () {
+        this.data = [];
+        this.push = function (info) {
+            var length = this.data.length;
+            this.data[length] = info;
+        }
+        this.pop = function () {
+            if (this.data) {
+                var length = this.data.length === 0 ? 0 : this.data.length - 1;
+                var item = this.data[length] ? true : false;
+                this.data.splice(length, 1);
+                return item;
+            }
+        }
+        this.reset = function () {
+            this.data = [];
+        }
+    };
+
+    var Execute = function (expr) {
+        var i = 0;
+        var left;
+        var right, operator, result;
+
+        while (expr[i] !== "+" || expr[i] !== "-" || expr[i] !== "*" || expr[i] !== "/" && i < expr.length) {
+            left = expr[i];
+            i++;
+        }
+        operator = expr[i++];
+        right = expr[i];
+        left = Number.parseInt(left);
+        right = Number.parseInt(right);
+        switch (operator) {
+            case "+":
+                result = left + right;
+                break;
+            case "-":
+                result = left - right;
+                break;
+            case "*":
+                result = left * right;
+                break;
+            case "/":
+                result = left / right;
+                break;
+            default:
+                result = "Not a valid statement";
+        }
+        return result;
+    };
+
+    var ExpressionEvaluator = function (expression) {
+        var arr = expression.split('');
+        console.log(Validator(arr));
+    }
+    var expResult = 0;
+    var Validator = function (arr) {
+        var stack = new Stack();
+        var popRes;
+        var intermediate;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === "{") {
+                stack.push("{");
+            }
+            else if (arr[i] === "}") {
+                if (!stack.pop()) {
+                    return false;
+                }
+            }
+            else {
+                intermediate += arr[i]
+
+            }
+        }
+
+        return stack.data.length === 0;
+      
+    }
+
+    var factory = {
+        Stack: Stack,
+        Execute: Execute(intermediate),
+        Validator: Validator,
+        ExpressionEvaluator: ExpressionEvaluator
+    };
+
+    return factory;
+
+});
+
+
+
+
+
+
+
